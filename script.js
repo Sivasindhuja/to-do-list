57ede934165ede3a3351311279dc16776683d0d8
Siva Sindhuja Tsundupalli <160160724+Sivasindhuja@users.noreply.github.com>
2026-01-06 21:44:31 +0530

    Added localStorage for task persistence
    Added task management features with localStorage support.

--- script.js
+++ script.js
@@ -1,6 +1,59 @@
 const addButton=document.getElementById("add-btn");
 const task=document.getElementById("task-input");
 const taskList=document.getElementById("task-list");
+
+const tasks=[];
+
+function loadTasks(){
+    const savedItems=JSON.parse(localStorage.getItem("tasks"));
+     if (savedItems) {
+        savedItems.forEach(task => {
+            tasks.push(task);
+            renderTask(task.text,task.completed);
+        });
+    }
+}
+
+function saveTasks(){
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+
+
+function renderTask(taskText,completed=false){
+    const li = document.createElement("li");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked=completed
+
+    checkbox.addEventListener("change", () => {
+    li.classList.toggle("completed");
+    toggleCompleted(taskText);
+});
+
+    const span = document.createElement("span");
+    span.textContent = taskText;
+
+    if (completed) {
+        li.classList.add("completed");
+    }
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "DELETE";
+
+    deleteButton.addEventListener("click", () => {
+        li.remove();
+        deleteTask(taskText);
+    });
+
+    li.append(checkbox,span, deleteButton);
+    taskList.appendChild(li);
+}
+
+//localStorage only stores data in the form of strings
+
+
+
 addButton.addEventListener("click",()=>{
 
     // remove white spaces or extra spaces
@@ -14,26 +67,37 @@ addButton.addEventListener("click",()=>{
         return;
     }
 
-    //create a new li element and add the formatted text to that li element
-
-    const li=document.createElement("li");
-    li.textContent=formattedText;
-
-    //add this newly created li element to the original task list
-    taskList.appendChild(li);
-
-    //create a space to show this added task
-    const span = document.createElement("span");
-    span.textContent = formattedText;
-
-    const deleteButton=document.createElement("button");
-    deleteButton.textContent="DELETE"
-    li.appendChild(deleteButton);
-
-    deleteButton.addEventListener('click',()=>{
-    li.remove();
-    //after removing any task,remove its corresponding delete button also from the DOM
-    deleteButton.remove();
+    tasks.push({
+        text:formattedText,
+        completed:false
+        
     })
+    saveTasks();
+    renderTask(formattedText,false);
     task.value="";
+  
+
 });
+function deleteTask(taskText){
+    const index=tasks.findIndex(
+        function(t){
+            return t.text===taskText;
+        }
+    )
+    if(index!==-1){
+        //remove that index from tasks array and save the new array to localStorage
+
+        tasks.splice(index,1);
+        saveTasks();
+    }
+}
+function toggleCompleted(taskText) {
+    const task = tasks.find(t => t.text === taskText);
+
+    if (task) {
+        task.completed = !task.completed;
+        saveTasks();
+    }
+}
+
+loadTasks();
